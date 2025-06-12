7851c0aee7fec9b1b314bb60ac0f6aa65a0fcbee
xandresariasx <128938922+xandresariasx@users.noreply.github.com>
2025-06-12 13:02:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -103,19 +103,19 @@ function speak(text) {
     const utterance = new SpeechSynthesisUtterance(text);
     
     // Prioritize voices for the selected language
-    //const voices = window.speechSynthesis.getVoices();
-    //let desiredVoice;
+    const voices = window.speechSynthesis.getVoices();
+    let desiredVoice;
     
     //if (language === 'es') {
-    //    desiredVoice = voices.find(v => v.lang === 'es-CO') || 
+        desiredVoice = voices.find(v => v.lang === 'es-CO');// || 
     //                   voices.find(v => v.lang === 'es-419') || 
     //                   voices.find(v => v.lang.startsWith('es-'));
     //} else {
     //    desiredVoice = voices.find(v => v.lang === 'en-US');
     //}
     
-    //utterance.voice = desiredVoice || voices[0];
-    //window.speechSynthesis.speak(utterance);
+    utterance.voice = desiredVoice || voices[0];
+    window.speechSynthesis.speak(utterance);
     synth.speak(utterance);
 }
 
